d624424af0ba17b1c5f6ba4d5f59f071f0af3232
Ross Parsons <rparsons779@gmail.com>
2023-08-10 23:45:08 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ross Parsons on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
